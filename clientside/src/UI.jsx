dd95d308644aa1f22c3596bfc0bdd413a962e710
Hemanth KR <krh7799@gmail.com>
2023-12-16 02:14:49 +0530

    Working on UI

---
+++ clientside/src/UI.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+function UI() {
+  return (
+    <>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Alegreya:ital@1&family=Amatic+SC:wght@700&family=Belanosima&family=Bree+Serif&family=Caveat:wght@700&family=Chakra+Petch:wght@500&family=Courier+Prime&family=Dosis&family=Edu+TAS+Beginner:wght@700&family=Exo+2:wght@300&family=Fasthand&family=Fredericka+the+Great&family=Handjet:wght@300&family=Indie+Flower&family=Inter:wght@300&family=Kalam&family=Lugrasimo&family=Lumanosimo&family=Montserrat+Alternates&family=Montserrat:wght@200;300;400&family=Nunito:wght@200&family=Pangolin&family=Rajdhani&family=Raleway:wght@300&family=Red+Hat+Display:wght@500&family=Rubik&family=Sacramento&family=Sarabun&family=Sedgwick+Ave+Display&family=Slabo+27px&family=Tajawal&family=Titillium+Web:wght@300&family=Ysabeau+Infant:wght@200&family=Ysabeau+SC:wght@300&family=Zeyada&display=swap');
+        {`.head{
+        height:120px;
+        // background-color:rgb(61, 26, 60);
+        font-family: handjet;
+        font-size:1cm;
+        letter-spacing:5px;
+        font-weight:100;
+        }
+
+        .title{
+            padding-top:0px;
+            color:red;
+        }
+
+
+        .body{
+            height:88vh;
+            background-color:rgb(61, 26, 60);
+        }
+
+        .title{
+            color:white;
+        }`}
+      </style>
+      <div className="main">
+        <div className="head">
+          <img src="" alt="" className="i1" />
+          <h2 className="title">C0!NFL!P</h2>
+        </div>
+        <div className="body">Hey</div>
+      </div>
+    </>
+  );
+}
+
+export default UI;
