fb472fdd0367c886443d1b9066ae309569ae9955
Hemanth KR <krh7799@gmail.com>
2023-12-15 18:01:00 +0530

    Added Openzepplin

---
+++ contracts/coinflip.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier : GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+
+// import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract CoinFlip {
+    // using safeMath for uint256;
+
+    function flip(bool guess) public view returns (bool) {}
+}
