8beef2445c1af36e5976fe82d68ded6f49a6e951
Hemanth KR <krh7799@gmail.com>
2023-12-15 18:14:58 +0530

    Contract Ready

--- contracts/coinflip.sol
+++ contracts/coinflip.sol
@@ -6,5 +6,34 @@ pragma solidity >=0.4.22 <0.9.0;
 contract CoinFlip {
     // using safeMath for uint256;
 
-    function flip(bool guess) public view returns (bool) {}
+    uint256 lastval;
+    uint256 public win;
+    uint256 fac =
+        123456789098765433456789065434567856783456789098765484567890098765434567890;
+
+    constructor() {
+        win = 0;
+    }
+
+    function flip(bool guess) public returns (bool) {
+        uint blk = uint256(blockhash(block.number.sub(1)));
+
+        if (lastval == blk) {
+            revert();
+        }
+
+        lastval = blk;
+
+        uint256 val = blk.div(fac);
+
+        bool side = val == 1 ? true : false;
+
+        if (side == guess) {
+            win++;
+            return true;
+        } else {
+            win--;
+            return false;
+        }
+    }
 }
