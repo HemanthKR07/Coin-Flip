32100d8ab85532bdbd9287123c01bcc5bcc450c3
byteZorvin <imehul24@gmail.com>
2023-12-29 20:03:52 +0530

    add withdrawal function

--- contracts/coinflip.sol
+++ contracts/coinflip.sol
@@ -6,6 +6,16 @@ contract coinFlip {
     uint256 public betval = 1e15; // The threshhold
     uint256 public botval;
 
+    uint256 private constant NOT_ENTERED = 1;
+    uint256 private constant ENTERED = 2;
+
+    uint256 private _status;
+
+    /**
+     * @dev Unauthorized reentrant call.
+     */
+    error ReentrancyGuardReentrantCall();
+
     constructor() payable {
         owner = msg.sender;
         botval = msg.value; // Bot balance
@@ -18,9 +28,31 @@ contract coinFlip {
         _;
     }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "should be the owner");
+        _;
+    }
+
+    modifier nonReentrant() {
+        _nonReentrantBefore();
+        _;
+        _status = NOT_ENTERED;
+    }
+
     modifier validbet() {
         require(msg.value >= betval, "Invalid bet value");
         _;
+    }
+
+    
+    function _nonReentrantBefore() private {
+        // On the first call to nonReentrant, _status will be NOT_ENTERED
+        if (_status == ENTERED) {
+            revert ReentrancyGuardReentrantCall();
+        }
+
+        // Any calls to nonReentrant after this point will fail
+        _status = ENTERED;
     }
 
     function flip(bool betOn) external payable botbalance validbet {
@@ -33,5 +65,12 @@ contract coinFlip {
         botval += msg.value;
 
         emit betPlaced(msg.sender, msg.value, prediction);
+    }
+
+    function withdrawWinnings() external onlyOwner nonReentrant {
+        uint256 contractBalance = address(this).balance;
+        require(botval == contractBalance, "check validity");
+        (bool sent, ) = owner.call{value: contractBalance}("");
+        require(sent, "Failed to send Ether");
     }
 }
